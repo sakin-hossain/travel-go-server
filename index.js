7e3254c781ad73558bb25d5c326acbe2d1508dfe
sakin-hossain <info.sakinh@gmail.com>
2021-10-30 12:34:18 +0600

    package add, order show for individual user added

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const app = express()
+var cors = require('cors')
+const port = process.env.PORT || 5000
+const bodyParser = require("body-parser");
+const ObjectId = require('mongodb').ObjectId; 
+
+require('dotenv').config()
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tqbro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db("travelGoDb");
+        const packagesCollection = database.collection("packages");
+        const emailCollection = database.collection("bookBYEmail");
+
+        // POST data
+        app.post('/addPackages', async (req,res)=>{
+            const result = await packagesCollection.insertOne(req.body);
+            res.send(result);
+        });
+        app.get('/addPackages', async (req,res)=>{
+            const result = await packagesCollection.find({}).toArray();
+            res.json(result);
+        });
+        // get package using is
+        app.get('/addPackages/:id', async (req,res)=>{
+            const query = { _id: ObjectId(req.params.id) }
+            const package = await packagesCollection.findOne(query);
+            res.send(package);
+        });
+
+        app.post('/myOrders', async(req,res)=>{
+            const result = await emailCollection.insertOne(req.body);
+            res.send(result);
+        });
+        app.get('/myOrders', async (req,res)=>{
+            const result = await emailCollection.find({}).toArray();
+            res.json(result);
+        });
+
+        app.delete('/myOrders/:id', async(req,res)=>{
+            const query = {_id: ObjectId(req.params.id)};
+            const result = await emailCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally{
+        // await
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
